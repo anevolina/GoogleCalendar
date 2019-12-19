caf213584abdcd44a3a31f53c708623227a94992
Nassstasia16 <nassstasia16@gmail.com>
2019-12-18 19:08:16 -0800

    test for get_start_end_time

---
+++ test_calendar_core.py
@@ -0,0 +1,27 @@
+import unittest
+from datetime import datetime
+
+
+from calendar_core import get_start_end_time
+
+class CalendarCoreTest(unittest.TestCase):
+
+    def test_get_start_end_time(self):
+
+        start1, end1 = get_start_end_time('27 Jul 2019 11 a.m.')
+        start2, end2 = get_start_end_time('April 19 2018 at 6 p.m.', 0.5)
+        start3, end3 = get_start_end_time('June')
+
+        self.assertEqual((start1, end1), (datetime(year=2019, month=7, day=27, hour=11, minute=0),
+                                          datetime(year=2019, month=7, day=27, hour=12, minute=0)))
+
+        self.assertEqual((start2, end2), (datetime(year=2018, month=4, day=19, hour=18, minute=0),
+                                          datetime(year=2018, month=4, day=19, hour=18, minute=30)))
+
+        self.assertEqual((start3, end3), (datetime.now().date(), datetime.now().date()))
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
