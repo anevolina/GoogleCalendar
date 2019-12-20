39158b33c7a3fd92fd1da96579519ac1061612b1
Nassstasia16 <nassstasia16@gmail.com>
2019-12-19 21:53:53 -0800

    transfer parsing function to another module

--- calendar_core.py
+++ calendar_core.py
@@ -3,19 +3,28 @@ from googleapiclient.discovery import build
 
 import datetime
 import pickle
-import datefinder
+import sqlite3
 
+savings = None
 
-def authorise():
+def authorise(user_id):
     scopes = ['https://www.googleapis.com/auth/calendar']
 
     flow = InstalledAppFlow.from_client_secrets_file("client_secret.json", scopes=scopes)
     credentials = flow.run_console()
 
-    dump_credentials(credentials)
+    save_credentials(user_id, credentials)
 
+def make_connection(database):
 
-def dump_credentials(credentials):
+    conn = None
+    try:
+        conn = sqlite3.connect(database)
+    except:
+        pass
+
+def save_credentials(user_id, credentials):
+
     pickle.dump(credentials, open("token.pkl", "wb"))
 
 
@@ -23,29 +32,9 @@ def get_credentials():
     return pickle.load(open("token.pkl", "rb"))
 
 
-def get_start_end_time(start_time: str, duration=1):
-
-    start_time_match = list(datefinder.find_dates(start_time))
-
-    if start_time_match and start_time_match[0].hour:
-        start_time = start_time_match[0]
-        end_time = (start_time_match[0] + datetime.timedelta(hours=duration))
-
-    elif start_time_match:
-        start_time = start_time_match[0].date()
-        end_time = start_time_match[0].date() + datetime.timedelta(days=duration)
-
-    else:
-        start_time = end_time = datetime.datetime.now().date()
-
-    return start_time, end_time
-
-
-def get_formated_start_end_time(start_time: str, duration=1):
+def get_formated_start_end_time(start_time, end_time):
 
     time_zone = get_timezone()
-
-    start_time, end_time = get_start_end_time(start_time, duration)
 
     start = {'timeZone': time_zone}
     end = {'timeZone': time_zone}
@@ -75,14 +64,14 @@ def get_calendar():
     return service
 
 
-def add_event(start_time, summary, duration=1, attendees=None, description=None, location=None):
+def add_event(user_id, description, start, end, attendees=None, location=None):
 
-    start, end = get_formated_start_end_time(start_time, duration)
+    start, end = get_formated_start_end_time(start, end)
 
     event = {
         'start': start,
         'end': end,
-        'summary': summary,
+        'summary': description[:50],
         'description': description,
         'location': location,
         'attendees': [{'email': email} for email in attendees] if attendees else None
@@ -93,7 +82,4 @@ def add_event(start_time, summary, duration=1, attendees=None, description=None,
     service.events().insert(calendarId='primary', body=event, sendNotifications=True).execute()
 
     return
-
-
-add_event('завтра', 'test summary', description='Test description')
 
