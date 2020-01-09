51c36075646f42a2e2dd6a2965e09b32ba633fac
Nassstasia16 <nassstasia16@gmail.com>
2020-01-09 15:52:40 -0800

    delete messages with auth

--- GC_TelegramBot.py
+++ GC_TelegramBot.py
@@ -1,24 +1,30 @@
 import bot_answers
-from main_api import create_event, add_calendar, unbind_calendar, authorise, authosire_user_step1, authosire_user_step2
+from main_api import create_event, add_calendar, unbind_calendar, authorise_user_step1, authorise_user_step2
 
 settings = {}
+messages = {}
 
 def handle_user_message(bot, update):
     user_id = get_user_id(bot, update)
     message = get_message(bot, update)
 
-    user_set = settings.get(user_id)
+    user_set = get_settings(user_id, 'code')
 
     if user_set == 'CALENDAR':
         calendar_status = add_calendar(user_id, message)
-        del settings[user_id]
         bot_answer = bot_answers.get_calendar_status_message(calendar_status, message)
+        del_settings(user_id)
 
     elif user_set == 'AUTHORISE':
-        authorised = authosire_user_step2(user_id, message)
+        authorised = authorise_user_step2(user_id, message)
+
         if authorised:
-            del settings[user_id]
             bot_answer = bot_answers.get_authorised_message()
+            message_id = update.message.message_id
+
+            del_auth_messages(bot, user_id, message_id)
+            del_settings(user_id)
+
         else:
             bot_answer = bot_answers.get_wrong_code_message()
 
@@ -29,22 +35,31 @@ def handle_user_message(bot, update):
     send_message(bot, user_id, bot_answer)
 
 
+def del_auth_messages(bot, user_id, message_id=None):
+    url_message = get_settings(user_id, 'url_message')
+
+    if message_id:
+        bot.delete_message(chat_id=user_id, message_id=message_id)
+
+    if url_message:
+        bot.delete_message(chat_id=user_id, message_id=url_message)
+
+    pass
+
+
 def add_calendar_callback(bot, update):
     user_id = get_user_id(bot, update)
-    settings[user_id] = 'CALENDAR'
+    add_settings(user_id, code='CALENDAR')
 
     bot_answer = bot_answers.get_add_calendar_message()
-
     send_message(bot, user_id, bot_answer)
 
 
 def chancel_callback(bot, update):
     user_id = get_user_id(bot, update)
 
-    try:
-        del settings[user_id]
-    except KeyError:
-        pass
+    del_auth_messages(bot, user_id)
+    del_settings(user_id)
 
     bot_answer = bot_answers.get_canceled_message()
     send_message(bot, user_id, bot_answer)
@@ -52,11 +67,30 @@ def chancel_callback(bot, update):
 def start_callback(bot, update):
     user_id = get_user_id(bot, update)
 
-    url = authosire_user_step1(user_id)
-
+    url = authorise_user_step1(user_id)
     bot_answer = bot_answers.get_authorise_url_message(url)
-    send_message(bot, user_id, bot_answer)
-    settings[user_id] = 'AUTHORISE'
+    message = send_message(bot, user_id, bot_answer)
+    add_settings(user_id, code='AUTHORISE', url_message=message.message_id)
+
+def add_settings(user_id, **kwargs):
+    settings[user_id] = {key: val for key, val in kwargs.items()}
+
+def del_settings(user_id):
+    try:
+        del settings[user_id]
+    except KeyError:
+        pass
+
+    return
+
+def get_settings(user_id, key=False):
+    user_set = settings.get(user_id)
+
+    if user_set and key:
+        key = user_set.get(key)
+        return key
+
+    return user_set
 
 
 def help_callback(bot, update):
@@ -74,7 +108,9 @@ def unbind_calendar_callback(bot, update):
 
 
 def send_message(bot, chat_id, message):
-    bot.send_message(chat_id=chat_id, text=message)
+    message = bot.send_message(chat_id=chat_id, text=message)
+
+    return message
 
 def get_user_id(bot, update):
     return update.message.from_user.id
